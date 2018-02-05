058d5482ad87a4c79b0730ec7e237f3d09722837
kazuki <neworder1988@i.spftbank.jp>
2018-02-05 22:49:58 +0900

    add login system

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,11 @@
 class BlogsController < ApplicationController
   before_action :set_params, only:[:show, :edit, :update, :destroy]
-  
+  before_action :logged_in?, only:[:new, :edit, :show]
+
   def index
     @blogs = Blog.all
   end
-  
+
   def new
     if params[:back]
       @blog = Blog.new(blog_params)
@@ -12,7 +13,7 @@ class BlogsController < ApplicationController
       @blog = Blog.new
     end
   end
-  
+
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
@@ -21,13 +22,13 @@ class BlogsController < ApplicationController
       render "new"
     end
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "ブログを編集しました！"
@@ -35,24 +36,31 @@ class BlogsController < ApplicationController
       render "edit"
     end
   end
-  
+
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice: "ブログを削除しました！"
   end
-  
+
   def confirm
     @blog = Blog.new(blog_params)
     render :new if @blog.invalid?
   end
-  
+
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
-  
+
   def set_params
     @blog = Blog.find(params[:id])
   end
-  
+
+  def logged_in?
+    if current_user.nil?
+      flash[:danger] = "ログインして下さい"
+      redirect_to new_session_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-    
+
+  get 'sessions/new'
+
   root to: 'tops#index'
-  
+
   resources :blogs do
     collection do
       post :confirm
     end
   end
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
 end
