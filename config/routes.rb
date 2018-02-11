7716461f883705e21480d2251e31eeb7954e412c
kazuki <neworder1988@i.spftbank.jp>
2018-02-11 18:54:49 +0900

    add favorites function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
   root to: 'tops#index'
 
   resources :blogs do
@@ -12,4 +10,5 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  resources :favorites, only: [:create, :destroy]
 end
